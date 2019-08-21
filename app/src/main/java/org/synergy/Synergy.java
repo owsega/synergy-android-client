06bccd55aa845d3a160b9c2cb7143b4675940660
oluwaseyi <seyi@sankore.com>
2019-08-21 11:31:42 +0100

    temporarily disable selinux to allow injection to work

--- app/src/main/java/org/synergy/Synergy.java
+++ app/src/main/java/org/synergy/Synergy.java
@@ -4,11 +4,11 @@
  * Copyright (C) 2010 The Synergy Project
  * Copyright (C) 2009 The Synergy+ Project
  * Copyright (C) 2002 Chris Schoeneman
- * 
+ *
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * found in the file COPYING that should have accompanied this file.
- * 
+ *
  * This package is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -18,19 +18,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.synergy;
-
-import org.synergy.base.Event;
-import org.synergy.base.EventQueue;
-import org.synergy.base.EventType;
-import org.synergy.base.Log;
-import org.synergy.client.Client;
-import org.synergy.common.screens.BasicScreen;
-//import org.synergy.common.screens.PlatformIndependentScreen;
-import org.synergy.injection.Injection;
-import org.synergy.net.NetworkAddress;
-import org.synergy.net.SocketFactoryInterface;
-import org.synergy.net.SynergyConnectTask;
-import org.synergy.net.TCPSocketFactory;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
@@ -43,120 +30,135 @@ import android.widget.EditText;
 
 import com.sdsmdg.tastytoast.TastyToast;
 
+import org.synergy.base.Event;
+import org.synergy.base.EventQueue;
+import org.synergy.base.EventType;
+import org.synergy.base.Log;
+import org.synergy.client.Client;
+import org.synergy.common.screens.BasicScreen;
+import org.synergy.injection.Injection;
+import org.synergy.net.NetworkAddress;
+import org.synergy.net.SocketFactoryInterface;
+import org.synergy.net.SynergyConnectTask;
+import org.synergy.net.TCPSocketFactory;
+
+//import org.synergy.common.screens.PlatformIndependentScreen;
+
 public class Synergy extends Activity {
-	
-	private final static String PROP_clientName = "clientName";
-	private final static String PROP_serverHost = "serverHost";
-	private final static String PROP_deviceName = "deviceName";
-	
-	private Thread mainLoopThread = null;
-	
-	static {
-		System.loadLibrary ("synergy-jni");
-	}
-	
-	private class MainLoopThread extends Thread {
-		
-		public void run () {
-			try {
-		        Event event = new Event ();
-		        event = EventQueue.getInstance ().getEvent (event, -1.0);
-		        Log.note ("Event grabbed");
-		        while (event.getType () != EventType.QUIT && mainLoopThread == Thread.currentThread()) {
-		            EventQueue.getInstance ().dispatchEvent (event);
-		            // TODO event.deleteData ();
-		            event = EventQueue.getInstance ().getEvent (event, -1.0);
-		            Log.note ("Event grabbed");
-		        } 
-				mainLoopThread = null;
-			} catch (Exception e) {
-				e.printStackTrace ();
-			} finally {
-				Injection.stop();
-			}
-		}
-	}
-	
-	
-    /** Called when the activity is first created. */
+
+    private final static String PROP_clientName = "clientName";
+    private final static String PROP_serverHost = "serverHost";
+    private final static String PROP_deviceName = "deviceName";
+
+    static {
+        System.loadLibrary("synergy-jni");
+    }
+
+    private Thread mainLoopThread = null;
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-       
+
         SharedPreferences preferences = getPreferences(MODE_PRIVATE);
         String clientName = preferences.getString(PROP_clientName, null);
         if (clientName != null) {
-        	((EditText) findViewById (R.id.clientNameEditText)).setText(clientName);
+            ((EditText) findViewById(R.id.clientNameEditText)).setText(clientName);
         }
-    	String serverHost = preferences.getString(PROP_serverHost, null);
+        String serverHost = preferences.getString(PROP_serverHost, null);
         if (serverHost != null) {
-        	((EditText) findViewById (R.id.serverHostEditText)).setText(serverHost);
+            ((EditText) findViewById(R.id.serverHostEditText)).setText(serverHost);
         }
-        
-        final Button connectButton = (Button) findViewById (R.id.connectButton);    
-        connectButton.setOnClickListener (new View.OnClickListener() {
-			public void onClick (View arg) {
-				connect ();
-			}
-		});
-        
-        Log.setLogLevel (Log.Level.INFO);
-		TastyToast.makeText(getApplicationContext(), "Client Starting", TastyToast.LENGTH_LONG, TastyToast.DEFAULT);
-        Log.debug ("Client starting....");
+
+        final Button connectButton = (Button) findViewById(R.id.connectButton);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg) {
+                connect();
+            }
+        });
+
+        Log.setLogLevel(Log.Level.INFO);
+        TastyToast.makeText(getApplicationContext(), "Client Starting", TastyToast.LENGTH_LONG, TastyToast.DEFAULT);
+        Log.debug("Client starting....");
 
         try {
-			Injection.setPermissionsForInputDevice();
-		} catch (Exception e) {
-			// TODO handle exception
-		}
-    }
-    
-    private void connect () {
-    	
-    	String clientName = ((EditText) findViewById (R.id.clientNameEditText)).getText().toString();
-    	String ipAddress = ((EditText) findViewById (R.id.serverHostEditText)).getText().toString();
-    	String portStr = ((EditText) findViewById(R.id.serverPortEditText)).getText().toString();
-    	int port = Integer.parseInt(portStr);
-    	String deviceName = ((EditText) findViewById(R.id.inputDeviceEditText)).getText().toString();
-    	
-    	SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-    	SharedPreferences.Editor preferencesEditor = preferences.edit();
-    	preferencesEditor.putString(PROP_clientName, clientName);
-    	preferencesEditor.putString(PROP_serverHost, ipAddress);
-    	preferencesEditor.putString(PROP_deviceName, deviceName);
-    	preferencesEditor.commit();
-    	
-        try {
-        	SocketFactoryInterface socketFactory = new TCPSocketFactory();
-       	   	NetworkAddress serverAddress = new NetworkAddress (ipAddress, port);
-
-        	Injection.startInjection(deviceName);
-
-        	BasicScreen basicScreen = new BasicScreen();
-        	
-        	WindowManager wm = getWindowManager();
-        	 
-        	Display display = wm.getDefaultDisplay ();
-        	basicScreen.setShape (display.getWidth (), display.getHeight ());
-        	
-        	
-        	//PlatformIndependentScreen screen = new PlatformIndependentScreen(basicScreen);
-            
-            Log.debug ("Hostname: " + clientName);
-            
-			Client client = new Client (getApplicationContext(), clientName, serverAddress, socketFactory, null, basicScreen);
-			new SynergyConnectTask().execute(client);
-			TastyToast.makeText(getApplicationContext(), "Device Connected", TastyToast.LENGTH_LONG, TastyToast.SUCCESS);
-
-			if (mainLoopThread == null) {
-				mainLoopThread = new MainLoopThread();
-				mainLoopThread.start();
-			}
-			
+            Injection.setPermissionsForInputDevice();
         } catch (Exception e) {
-        	e.printStackTrace();
-			TastyToast.makeText(getApplicationContext(), "Connection Failed", TastyToast.LENGTH_LONG, TastyToast.ERROR);
+            // TODO handle exception
+        }
+    }
+
+    private void connect() {
+
+        String clientName = ((EditText) findViewById(R.id.clientNameEditText)).getText().toString();
+        String ipAddress = ((EditText) findViewById(R.id.serverHostEditText)).getText().toString();
+        String portStr = ((EditText) findViewById(R.id.serverPortEditText)).getText().toString();
+        int port = Integer.parseInt(portStr);
+        String deviceName = ((EditText) findViewById(R.id.inputDeviceEditText)).getText().toString();
+
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor preferencesEditor = preferences.edit();
+        preferencesEditor.putString(PROP_clientName, clientName);
+        preferencesEditor.putString(PROP_serverHost, ipAddress);
+        preferencesEditor.putString(PROP_deviceName, deviceName);
+        preferencesEditor.commit();
+
+        try {
+            SocketFactoryInterface socketFactory = new TCPSocketFactory();
+            NetworkAddress serverAddress = new NetworkAddress(ipAddress, port);
+
+            Injection.startInjection(deviceName);
+
+            BasicScreen basicScreen = new BasicScreen();
+
+            WindowManager wm = getWindowManager();
+
+            Display display = wm.getDefaultDisplay();
+            basicScreen.setShape(display.getWidth(), display.getHeight());
+
+
+            //PlatformIndependentScreen screen = new PlatformIndependentScreen(basicScreen);
+
+            Log.debug("Hostname: " + clientName);
+
+            Client client = new Client(getApplicationContext(), clientName, serverAddress, socketFactory, null, basicScreen);
+            new SynergyConnectTask().execute(client);
+            TastyToast.makeText(getApplicationContext(), "Device Connected", TastyToast.LENGTH_LONG, TastyToast.SUCCESS);
+
+            if (mainLoopThread == null) {
+                mainLoopThread = new MainLoopThread();
+                mainLoopThread.start();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            TastyToast.makeText(getApplicationContext(), "Connection Failed", TastyToast.LENGTH_LONG, TastyToast.ERROR);
+        }
+    }
+
+    private class MainLoopThread extends Thread {
+
+        public void run() {
+            try {
+                Event event = new Event();
+                event = EventQueue.getInstance().getEvent(event, -1.0);
+                Log.note("Event grabbed");
+                while (event.getType() != EventType.QUIT && mainLoopThread == Thread.currentThread()) {
+                    EventQueue.getInstance().dispatchEvent(event);
+                    // TODO event.deleteData ();
+                    event = EventQueue.getInstance().getEvent(event, -1.0);
+                    Log.note("Event grabbed");
+                }
+                mainLoopThread = null;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                Injection.stop();
+            }
         }
     }
 }
--- app/src/main/java/org/synergy/injection/Injection.java
+++ app/src/main/java/org/synergy/injection/Injection.java
@@ -1,49 +1,75 @@
 package org.synergy.injection;
 
-import java.io.DataOutputStream;
-
 import org.synergy.base.Log;
+
+import java.io.DataOutputStream;
 
 public final class Injection {
 
-	static {
-		System.loadLibrary ("synergy-jni");
-	}
-	
-	/**
-	 * Functions imported from synergy-jni library
-	 */
-	public static final native void start (String deviceName);
-	public static native void stop ();
-	public static native void keydown (int key, int mask);
-	public static native void keyup (int key, int mask);
-	public static final native void movemouse (final int x, final int y);
-	public static native void mousedown (int buttonId);
-	public static native void mouseup (int buttonId);
-	public static native void mousewheel (int x, int y);
-	
-	private Injection () { }
-	
-	public static void setPermissionsForInputDevice () {
-		Log.debug ("Starting injection");
-		try {
-			Process process = Runtime.getRuntime ().exec ("su");
-			DataOutputStream dout = new DataOutputStream (process.getOutputStream ());
-			dout.writeBytes ("chmod 666 /dev/uinput");
-			dout.flush ();
-			dout.close ();
-			process.waitFor ();
-			Log.debug ("Access to /dev/uinput granted");
-		} catch (Exception e) {
-			e.printStackTrace ();
-		}
-	}
+    static {
+        System.loadLibrary("synergy-jni");
+    }
 
-	public static void startInjection (String deviceName) {
-		start (deviceName);
-	}
+    private Injection() {
+    }
 
-	public static void stopInjection () {
-		stop ();
-	}
+    public static native void keydown(int key, int mask);
+
+    public static native void keyup(int key, int mask);
+
+    public static final native void movemouse(final int x, final int y);
+
+    public static native void mousedown(int buttonId);
+
+    public static native void mouseup(int buttonId);
+
+    public static native void mousewheel(int x, int y);
+
+    public static void setPermissionsForInputDevice() {
+        Log.debug("Starting injection");
+        try {
+            Process process = Runtime.getRuntime().exec("su");
+            DataOutputStream dout = new DataOutputStream(process.getOutputStream());
+            dout.writeBytes("chmod 666 /dev/uinput");
+            dout.flush();
+            dout.close();
+            process.waitFor();
+            Log.debug("Access to /dev/uinput granted");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void startInjection(String deviceName) {
+        changeSELinuxMode(false);
+        start(deviceName);
+        changeSELinuxMode(true);
+    }
+
+    private static void changeSELinuxMode(boolean enable) {
+        int code = enable ? 1 : 0;  // setenforce 1 == enable/enforce selinux while 0 is to disable
+        Log.debug("Changing SELinux Mode to " + code);
+        try {
+            Process process = Runtime.getRuntime().exec("su");
+            DataOutputStream dout = new DataOutputStream(process.getOutputStream());
+            dout.writeBytes("setenforce " + code);
+            dout.flush();
+            dout.close();
+            process.waitFor();
+            Log.debug("SELinux Mode successfully changed");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Functions imported from synergy-jni library
+     */
+    public static final native void start(String deviceName);
+
+    public static void stopInjection() {
+        stop();
+    }
+
+    public static native void stop();
 }
